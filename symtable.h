80801148877c7164bae1b651b3a73690e2856682
Jared K <111449156+jarod137@users.noreply.github.com>
2024-01-25 22:44:42 -0700

    Create symtable.h

---
+++ symtable.h
@@ -0,0 +1,67 @@
+/*  Symbol Table --linked list  headers
+    Used for Compilers class
+
+    Modified Spring 2015 to allow for name to pointed to by symtable, instead of copied, since the name is copied
+    into the heap
+
+    Modified to have levels.  A level 0 means global variable, other levels means in range of the function.  We
+    start out our offsets at 0 (from stack pointer) for level 1,,,when we enter a functional declaration.
+    We increment offset each time we insert a new variable.  A variable is considered to be valid if it is found in
+    the symbol table at our level or lesser level.  If at 0, then it is global.  
+
+    We return a pointer to the symbol table when a variable matches our creteria.
+
+    We add a routine to remove variables at our level and above.
+
+    Jared Kaiser Spring 2023
+*/
+
+    
+
+
+#ifndef _SYMTAB 
+#define _SYMTAB
+
+#include "ast.h"
+
+enum  SYMBOL_SUBTYPE
+{
+   SYM_SCALAR,
+   SYM_ARRAY,
+   SYM_FUNCTION
+   // missing for array as parameter
+};
+
+void Display();
+int Delete();
+
+
+struct SymbTab
+{
+     char *name;
+     int offset; /* from activation record boundary */
+     int mysize;  /* number of words this item is 1 or more */
+     int level;  /* the level where we found the variable */
+     enum AST_MY_DATA_TYPE Declared_Type;  /* the type of the symbol */
+     int SubType;  /* the subtype of the symbol */
+     char *symbol;
+     ASTnode * fparms; /* pointer to parameters of the function in the AST */
+     char *label;
+
+     struct SymbTab *next;
+};
+
+
+struct SymbTab * Insert(char *name, enum AST_MY_DATA_TYPE my_assigned_type , enum  SYMBOL_SUBTYPE sub_type, int  level, int mysize, int offset);
+
+struct SymbTab * Search(char name[], int level, int recur );
+
+
+static struct SymbTab *first=NULL;   /* global pointers into the symbol table */
+
+char * CreateTemp();
+
+
+
+#endif
+
